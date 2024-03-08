b648bd7a7a778a489a870f81160835fbfaadb8a3
Ashley Nord <anord@hntb.com>
2024-03-08 16:49:38 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
 import ContactMe from "./components/ContactMe";
-import {Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
-  const [load, upadateLoad] = useState(true);
   return (
   <>
     <Navbar />
     <Routes>
-    <Route path="/" element={<AboutMe />} />
+      <Route path="/" element={<AboutMe />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/aboutme" element={<AboutMe />} />
       <Route path="/contactme" element={<ContactMe />} />
