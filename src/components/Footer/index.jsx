49e0c05becfeb34f515a701c10d95d0373b9ae62
Ashley Nord <anord@hntb.com>
2024-03-09 21:54:19 -0500

    styled the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,54 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
 export default function Footer(){  
+    let date = new Date();
+    let year = date.getFullYear();
     return (
-        <div>
-            <h1>
-                Footer
-            </h1>
-        </div>
-    )
-}
+        <Container className="footer">          
+            <div className="credit">
+              <h3>by Ashley Nord</h3>
+            </div>
+
+            <div className="copywright">
+              <h3>Copyright © {year}</h3>
+            </div>
+
+            <div className="links">              
+                <div className="social">
+                  <a
+                    href="https://github.com/a-nord"
+                    target="_blank" 
+                  >
+                    <AiFillGithub />
+                  </a>
+                </div>
+
+                <div className="social">
+                  <a
+                    href="https://www.linkedin.com/in/ashleynord/"
+                    target="_blank"                 
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </div>
+
+                <div className="social">
+                  <a
+                    href="https://www.instagram.com/ashley.s.l.n"
+                    target="_blank" 
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </div>              
+            </div>          
+        </Container>
+      );
+    }
+    
