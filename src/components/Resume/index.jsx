0ce8167638b0ef00e788d93049166d6054521f7d
Ashley Nord <anord@hntb.com>
2024-03-10 18:52:43 -0400

    fixing the resume page

---
+++ src/components/Resume/index.jsx
@@ -0,0 +1,21 @@
+// import pdf from "../../assets/AshleyNord_Resume.png"
+// import Container from 'react-bootstrap/Container';
+
+// export default function resume() {
+//     return (
+//         <Container>
+//         <img src={ pdf } alt="resume" className="resume" />
+//         </Container>
+//     );
+//     }
+
+// import React from 'react';
+
+// const PDFViewer = () => {
+//  return (
+//  <div className='resume'>
+//  <iframe src="../../assets/AshleyNord_Resume.pdf" width="1000px" height="100px"  />
+//  </div>
+//  );
+// };
+// export default PDFViewer;
