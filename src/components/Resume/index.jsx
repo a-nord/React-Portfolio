235b2f54fbe1acb6e9a955460a2bfb5804050089
Ashley Nord <anord@hntb.com>
2024-03-10 21:24:01 -0400

    updated resume page to have button and image of resume

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,13 +1,28 @@
-// import pdf from "../../assets/AshleyNord_Resume.png"
-// import Container from 'react-bootstrap/Container';
+import image from "../../assets/AshleyNord_Resume.png"
+import pdf from "../../assets/AshleyNord_Resume.pdf";
+import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
 
-// export default function resume() {
-//     return (
-//         <Container>
-//         <img src={ pdf } alt="resume" className="resume" />
-//         </Container>
-//     );
-//     }
+export default function resume() {
+    return (
+        <Container>
+            <div className="btn">
+            <Button href={pdf} target="_blank" style={{ maxWidth: "250px" }} >
+                <AiOutlineDownload />&nbsp;Download Resume
+            </Button>
+            </div>
+        
+            <img src={ image } alt="resume" className="resume" />
+
+            <div className="btn2">
+            <Button href={pdf} target="_blank" style={{ maxWidth: "250px" }} >
+                <AiOutlineDownload />&nbsp;Download Resume
+            </Button>
+            </div>
+        </Container>
+    );
+    }
 
 // import React from 'react';
 
