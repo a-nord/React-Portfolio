2040b43e77981822afcc42fac9a55d8e2a45cca5
a-nord <anord99@yahoo.com>
2024-03-11 12:49:58 -0400

    worked on some styling debuging

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,9 +1,35 @@
 export default function AboutMe(){  
     return (
+        <>        
         <div>
             <h1>
-                AboutMe
+                About Me
             </h1>
         </div>
+        
+        <div>
+            <h2>
+                Book of the Month:
+            </h2>
+        </div>
+
+        <div>
+            <h2>
+                Show of the Month:
+            </h2>
+        </div>
+
+        <div>
+            <h2>
+                Song of the Month:
+            </h2>
+        </div>
+
+        <div>
+            <h2>
+                Recent Travels:
+            </h2>
+        </div>
+        </>
     )
 }
