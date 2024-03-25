269ce96982007e88eadd7eec433acd8dfcc828d9
a-nord <anord99@yahoo.com>
2024-03-25 08:26:56 -0400

    fixed the text on image

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,6 +1,7 @@
 import headshot from '../../assets/headshot.png';
-import { Link } from "react-router-dom";
-import Carousel from '../../components/Carousel'
+import heart from '../../assets/items-container.png';
+// import { Link } from "react-router-dom";
+// import Carousel from '../../components/Carousel'
 
 export default function AboutMe(){  
     return (
@@ -12,44 +13,40 @@ export default function AboutMe(){
             </h1>
 
             <section className='aboutSection'>
-                <img src={ headshot } alt="ashley nord's recent headshot" className="headshot" />                                                             
-                <p className='myParagraph'>Welcome, Everyone! I'm Ashley Nord and I am from Boca Raton, Florida.  I am of Haitian descent and I am proficient in both English and Creole.  In my leisure time, I find solace in the realms of literature, physical exercise, and travel.  I love animals, my particular affection extends to cats and elephants.
-                    <br></br>
-                    <br></br>
+                <img src={ headshot } alt="ashley nord's recent headshot" className="headshot" />
+                <div className='hearts'>
+                    <img src={ heart } alt="ashley nord's about me background" className="heart" />                                                                             
+                    <p className='myParagraph'>Welcome, Everyone! I'm Ashley Nord and I am from Boca Raton, Florida.  I am of Haitian descent and I am proficient in both English and Creole.  In my leisure time, I find solace in the realms of literature, physical exercise, and travel.  I love animals, my particular affection extends to cats and elephants.
+                        <br></br>                        
                     Below, you'll discover which book has captured my attention, which show has become my latest obsession, the song I can't stop playing on repeat, and any recent destinations I've traveled to.
-                </p>                                                                               
+                    </p>
+                </div>                                                                               
             </section>
         </div>
-        <section>
-        <div className="aboutMeSection">
-        <Link className="aboutMeItems" to="/aboutme/book-of-month">
-            <h2>
-                Book of the Month
-            </h2>
-        </Link>
+        <section className="aboutMeSection">
+        <div >
+        {/* <Link className="aboutMeItems" to="/aboutme/book-of-month"> */}
+            <h2>Book of the Month</h2>
+        {/* </Link> */}
 
-        <Link className="aboutMeItems" to="/aboutme/show-of-month">
-            <h2>
-                Show of the Month
-            </h2>
-        </Link>
+        {/* <Link className="aboutMeItems" to="/aboutme/show-of-month"> */}
+            <h2>Show of the Month</h2>
+        {/* </Link> */}
 
-        <Link className="aboutMeItems" to="/aboutme/song-of-month">
-            <h2>
-                Song of the Month
-            </h2>
-        </Link>
+        {/* <Link className="aboutMeItems" to="/aboutme/song-of-month"> */}
+            <h2>Song of the Month</h2>
+        {/* </Link> */}
         </div>
         </section>
 
-        <div className='travels'>
+        {/* <div className='travels'>
             <h2>
                 Recent Travels
             </h2>
             <div>
             <Carousel />
             </div>
-        </div>
+        </div> */}
         </div>
         </>
     )
