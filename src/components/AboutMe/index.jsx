c9d7b9a657eec6fcc1dc85c5a7538c856b3eb5b7
a-nord <anord99@yahoo.com>
2024-04-18 15:44:09 -0400

    updated only books to show and responsive

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,5 +1,8 @@
 import headshot from '../../assets/headshot.png';
 import heart from '../../assets/items-container.png';
+import book from '../../assets/interests/the-fine-print.jpg'
+// import show from '../../assets/interests/the-odyssey.jpg'
+// import song from '../../assets/interests/Bakar_-_Hell_n_Back.png'
 // import { Link } from "react-router-dom";
 import Carousel from '../../components/Carousel'
 
@@ -17,8 +20,9 @@ export default function AboutMe(){
                 <div className='hearts'>
                     <img src={ heart } alt="ashley nord's about me background" className="heart" />                                                                             
                     <p className='myParagraph'>Welcome, Everyone! I'm Ashley Nord and I am from Boca Raton, Florida.  I am of Haitian descent and I am proficient in both English and Creole.  In my leisure time, I find solace in the realms of literature, physical exercise, and travel.  I love animals, my particular affection extends to cats and elephants.
-                        <br></br>                        
-                    Below, you'll discover which book has captured my attention, which show has become my latest obsession, the song I can't stop playing on repeat, and any recent destinations I've traveled to.
+                        <br></br>      
+                        <br></br>                    
+                    Below, you'll discover which book has currently captured my attention, and any recent destinations I've traveled to.
                     </p>
                 </div>                                                                               
             </section>
@@ -27,41 +31,43 @@ export default function AboutMe(){
         <section className="aboutMeSection">        
         <div className="aboutMeItems" >
             <img src={ heart } alt="ashley nord's about me background" className="mini-heart" />
-            <h2 className='interest'>Book of the Month</h2>
+            <div className='interest'>
+                <h2>Book of the Month</h2>
+                <img src={book} alt="cover of the fine print by lauren asher" className='book'/>
+            </div>
         </div>
 
-        <div className="aboutMeItems" >
+        {/* <div className="aboutMeItems" >
             <img src={ heart } alt="ashley nord's about me background" className="mini-heart" />
-            <h2 className='interest'>Show of the Month</h2>
-        </div>
+            <div className='interest'> 
+                <h2 className='showTitle'>Show of the Month</h2>
+                <img src={show} alt="cover of the greek mythology" className='show'/>
+            </div>
 
-        <div className="aboutMeItems" >
+        </div> */}
+
+        {/* <div className="aboutMeItems" >
             <img src={ heart } alt="ashley nord's about me background" className="mini-heart" />
-            <h2 className='interest'>Song of the Month</h2>
-        </div>        
+            <div className='interest'>
+                <h2>Song of the Month</h2>
+                <img src={song} alt="cover of the greek mythology" className='song'/>
+            </div>
+        </div>         */}
         </section>
 
-        <div className='travels'>
+{/* Update after tavel dates */}
+        {/* <div className='travels'>
             <h2>
                 Recent Travels
             </h2>
+            
             <div>
             <Carousel />
             </div>
-        </div>
+        </div> */}
         </div>
         </>
     )
 }
 
-{/* <div className="aboutMeItems" to="/aboutme/book-of-month">
-            <h2>Book of the Month</h2>
-        </div>
 
-        <div className="aboutMeItems" to="/aboutme/show-of-month">
-            <h2>Show of the Month</h2>
-        </div>
-
-        <div className="aboutMeItems" to="/aboutme/song-of-month">
-            <h2>Song of the Month</h2>
-        </div> */}
