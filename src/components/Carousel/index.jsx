71d08b54b76985f135ef227a7a0e92ab1bf5a70f
a-nord <anord99@yahoo.com>
2024-04-18 14:32:58 -0400

    completed the carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import headshot from '../../assets/items-container.png';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 // import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import './carousel.css';
 import { Carousel } from 'react-responsive-carousel';
 
 class DemoCarousel extends Component {
