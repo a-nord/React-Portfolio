bb6dbb9a6b5d15e00ec06006c38e1e86b29e5e31
a-nord <anord99@yahoo.com>
2024-04-21 23:43:51 -0400

    projects section

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -8,16 +8,61 @@ import {
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import litlit from '../../assets/projects/litlit.png';
+import animeotaku from '../../assets/projects/animeotaku.png';
+import dndgo from '../../assets/projects/dndgo.png';
+import ProjectCard from "./projectCard";
+
+
 
 
 
 export default function Portfolio(){  
     return (    
         <div>
+            <section>
             <h1 className="aboutMe">
                 Projects
             </h1>
+            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={3} className="project-card">
+            <ProjectCard
+              imgPath={animeotaku}
+              isBlog={false}
+              title="Anime Otaku"
+              description="This application provides an interactive anime theme search.  The user will be able to see information about the artist as well as a few more songs from the artist if they are interested. "
+              ghLink="https://github.com/a-nord/Anime-Otaku"
+              demoLink="https://a-nord.github.io/Anime-Otaku/"
+            />
+            </Col>
 
+            <Col md={3} className="project-card">
+            <ProjectCard
+              imgPath={dndgo}
+              isBlog={false}
+              title="DnD GO"
+              description="This MERN application is designed for the dungeons and dragons community to gather and connect. D&D-GO is an immersive game inspired by Dungeons & Dragons.  This is an interactive front-end group project."
+              ghLink="https://github.com/Aricoria10/DnD-GO"
+              demoLink="https://dnd-go.onrender.com/"
+            />
+            </Col>
+
+            <Col md={3} className="project-card">
+            <ProjectCard
+              imgPath={litlit}
+              isBlog={false}
+              title="LitLit"
+              description="This application is designed for the bookworm community to gather and connect. Users can create an account to access the app's features. This project was built using Express, Handlebars, mySQL2, sequelize, and tailwind css."
+              ghLink="https://github.com/a-nord/Lit-Lit"
+              demoLink="https://lit-lit-5480ca7bfcf5.herokuapp.com/login"
+            />
+            </Col>
+            </Row>         
+            
+            </section>
+
+
+            <section className="skills">
             <h2 className="socials">
                 Skills
             </h2>
@@ -37,6 +82,7 @@ export default function Portfolio(){
                 </Col>
             </Row>
             </Container>
+            </section>
 
         </div>
         
